dfdb9de548e15ac706d460a20a6dcaeb26eb7773
Behruzbekolimov <olimovbehruzbek@gmail.com>
2023-01-09 12:16:28 +0500

    Initial Commit

---
+++ src/main/java/com/example/springtelegrambotstarter/SpringTelegramBotStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtelegrambotstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTelegramBotStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTelegramBotStarterApplication.class, args);
+    }
+
+}
